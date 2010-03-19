1f47fb5f138b015a2c6a2a1745ac879d6f8ed451
Nathan Froyd <froydnj@gmail.com>
2010-03-18 23:42:30 -0400

    add OCTET-VECTOR type

--- types.lisp
+++ types.lisp
@@ -4,6 +4,9 @@
 
 (deftype index () '(mod #.array-dimension-limit))
 
+(deftype octet-vector (&optional (length '*))
+  `(array (unsigned-byte 8) (,length)))
+
 (deftype simple-octet-vector (&optional (length '*))
   #+(or sbcl cmu) `(simple-array (unsigned-byte 8) (,length))
   #-(or sbcl cmu) `(array (unsigned-byte 8) (,length)))
