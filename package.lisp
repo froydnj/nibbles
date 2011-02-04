637c3b3a4c852d0a747d29f71eef52472bf2682e
Nathan Froyd <froydnj@gmail.com>
2011-02-04 13:46:55 -0500

    fix exports of readers and writers

--- package.lisp
+++ package.lisp
@@ -9,10 +9,10 @@
   (:export #:ieee-single-ref/be #:ieee-single-ref/le
            #:ieee-double-ref/be #:ieee-double-ref/le)
   ;; Stream readers.
-  (:export #:read-ub16/le #:read-ub16/be #:read-sb16/le #:read-ub16/be
-           #:read-ub32/le #:read-ub32/be #:read-sb32/le #:read-ub32/be
-           #:read-ub64/le #:read-ub64/be #:read-sb64/le #:read-ub64/be)
+  (:export #:read-ub16/le #:read-ub16/be #:read-sb16/be #:read-sb16/le
+           #:read-ub32/le #:read-ub32/be #:read-sb32/be #:read-sb32/le
+           #:read-ub64/le #:read-ub64/be #:read-sb64/be #:read-sb64/le)
   ;; Stream writers.
-  (:export #:write-ub16/le #:write-ub16/be #:write-sb16/le #:write-ub16/be
-           #:write-ub32/le #:write-ub32/be #:write-sb32/le #:write-ub32/be
-           #:write-ub64/le #:write-ub64/be #:write-sb64/le #:write-ub64/be))
+  (:export #:write-ub16/le #:write-ub16/be #:write-sb16/be #:write-sb16/le
+           #:write-ub32/le #:write-ub32/be #:write-sb32/be #:write-sb32/le
+           #:write-ub64/le #:write-ub64/be #:write-sb64/be #:write-sb64/le))
